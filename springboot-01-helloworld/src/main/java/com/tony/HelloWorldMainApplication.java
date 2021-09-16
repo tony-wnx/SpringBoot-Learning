9d0ea428e2a67563690d31a76917382ba651dd2e
yxq <xiongqian.yang@qunar.com>
2021-09-16 17:25:20 +0800

    up

--- springboot-01-helloworld/src/main/java/com/tony/HelloWorldMainApplication.java
+++ springboot-01-helloworld/src/main/java/com/tony/HelloWorldMainApplication.java
@@ -9,6 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldMainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldMainApplication.class,args);
+        SpringApplication.run(HelloWorldMainApplication.class, args);
     }
 }
